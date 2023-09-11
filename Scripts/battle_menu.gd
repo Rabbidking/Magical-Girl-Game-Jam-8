6e960e9973c33c4eb623d2c0f4c62d11bfd76026
John Bickel <bickel.john@yahoo.com>
2023-09-11 18:42:00 -0400

    Removed redundant scenes/scripts

--- Scripts/battle_menu.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-
-class_name Battle_Menu
-
-@export var battle_options: Array[Battle_Options]
--- Scripts/battle_options.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name Battle_Options
-
-@export var name: String = ""
-@export var texture: Texture2D
-
-
